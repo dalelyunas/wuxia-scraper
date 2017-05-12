90f249cd6454e2c1eb2bdb961a3d667598344fea
dalelyunas <alelyunas.d@husky.neu.edu>
2017-05-11 20:13:34 -0400

    flag to send email

--- wuxia-scraper/run_scraper.py
+++ wuxia-scraper/run_scraper.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
+import json
 from scrape_books import scrape_all_books
 from build_pdfs import convert_all_books
 from email_kindle import send_all_books
+
+config = json.load(open('../config.json'))
 
 
 def run_scraper():
     scrape_all_books()
     convert_all_books()
-    send_all_books()
+    if config['email_enabled']:
+        send_all_books()
 
 if __name__ == "__main__":
         run_scraper()
